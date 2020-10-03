c43e109c6c19e72ea13b0cb12938718b9d0b7e51
Adam Spice <adwasere@gmail.com>
2020-10-04 06:46:22 +1300

    implemented sign in form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import Header from './components/Header'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
+import LoginScreen from './screens/LoginScreen'
 
 const App = () => {
   return (
@@ -15,9 +16,10 @@ const App = () => {
         <Header />
         <Container>
           <main className='py-3'>
-            <Route path='/' component={HomeScreen} exact />
+            <Route path='/login' component={LoginScreen} />
             <Route path='/product/:id' component={ProductScreen} />
             <Route path='/cart/:id?' component={CartScreen} />
+            <Route path='/' component={HomeScreen} exact />
           </main>
         </Container>
         <Footer />
